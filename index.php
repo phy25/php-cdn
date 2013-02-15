a0d4711ab0fdb704c3345efe9d643457643aee8e
Phy <git@phy25.com>
2013-02-15 18:38:11 +0800

    I love it when I use an afternoon without stop to make the useable version.

--- index.php
+++ index.php
@@ -2,41 +2,56 @@
 /*
  * Forked from https://github.com/trigunflame/php-cdn and modified by Phy25
  *
- * *** Milestone: This is a passive CDN service, but what I need is an active one. ***
- * php-cdn
- * dynamic file caching pseudo cdn
+ * *** DOING Milestone: This is a passive CDN service, but what I need is an active one. ***
+ * This script will sync the accessed file.
  *
- * cdn root path   : http://cdn.com/
- * cdn example url : http://cdn.com/path/to/resource.css?d=12345
- * maps the uri    : /path/to/resource.css?d=12345
- * to the origin   : http://yoursite.com/path/to/resource.css?d=12345
- * caches file to  : ./cache/[base64-encoded-uri].css
- * returns local cached copy or issues 304 not modified
+ * server -> this script -> remote
  */
 
-// error_reporting(E_ALL);
+error_reporting(E_ALL);
+
+require_once('./upyun.class.php');
+
+require_once('./config.inc.php');
+
+$upyun = new UpYun(UPYUN_BUCKET, UPYUN_USERNAME, UPYUN_USERPASS, UpYun::ED_TELECOM);
+$f_remote_publicprefix = 'http://phy25-cdn.b0.upaiyun.com';
+
+$f_remote_path = '/';
+
 
 // files.lst location
 $f_listloc = './files.lst';
 /*
  * File Format:
- * local_path remote_path caching_time(d = default, f = forever, (int)/second)
+ * local_path server_path caching_time
+ * @param cachingtime 
+ *  	d = default;
+ *  	f = forever;
+ *  	n = no mtime check, use the default time;
+ *  	(int)/second
  */
 
 $f_relative_path = '/remote/';
 
 // default caching time (N seconds) (86400s = 1d)
-$f_expires_default = 86400;
+$f_expires_default = 60;
 
 // make out the right path
 $req_file = str_replace($f_relative_path, '', $_SERVER['REQUEST_URI']);
+
+$req_filename = parse_url($req_file, PHP_URL_PATH);
 
 // encode as filename-safe base64 for the cache name
 $f_name = strtr(base64_encode($req_file), '+/=', '-_,');
 
 // parse the file extension
-$f_ext = strrchr(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '.');
+$f_ext = strrchr($req_filename, '.');
 
+// do not compare the file with the server; just pull from the server (turn it on only when you are testing!)
+$force_sync = false;
+
+// to ignore the mtime check, use the list file to configure
 
 if(!file_exists($f_listloc)){
 	header($_SERVER['SERVER_PROTOCOL'] . ' 500 Server Error');
@@ -46,15 +61,19 @@ if(!file_exists($f_listloc)){
 $f_listh = fopen($f_listloc, 'r');
 
 $f_matched = null;
-while(!feof($f_listh)){
+while(!feof($f_listh)){// match the accessed url with the list
 	$f_listl = explode(' ', fgets($f_listh));
 	if(substr($f_listl[0], -1, 1) == '/' && strpos($req_file, $f_listl[0]) === 0){ // in folder mode we check if the url match the rule 
 		$f_matched = $f_listl;
-		$f_remote_uri = $f_listl[1].str_replace($f_listl[0], '', $req_file);
+		$server_uri = $f_listl[1].str_replace($f_listl[0], '', $req_file);
+		$remote_uri = $f_remote_path.$req_file;
+		$remote_publicuri = $f_remote_publicprefix.$f_remote_path.$req_file;
 		break; // exit the loop
 	}elseif($req_file == $f_listl[0]){ // file mode
 		$f_matched = $f_listl;
-		$f_remote_uri = $f_listl[1];
+		$server_uri = $f_listl[1];
+		$remote_uri = $f_remote_path.$req_filename; // when syncing to remote, ?key=value is useless
+		$remote_publicuri = $f_remote_publicprefix.$f_remote_path.$req_file;
 		break; // exit the loop
 	}
 	// if it doesn't match the line, go to the next line
@@ -62,7 +81,7 @@ while(!feof($f_listh)){
 
 fclose($f_listh);
 
-//var_dump($f_matched, $f_remote_uri);
+//var_dump($f_matched, $server_uri);
 
 if(!is_array($f_matched)){ // if it matches nothing
 	header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
@@ -71,29 +90,10 @@ if(!is_array($f_matched)){ // if it matches nothing
 
 $f_matched[2] = trim($f_matched[2]);
 
-// assign the correct mime type
-switch ($f_ext) {
-	// images
-	case '.gif'  : $f_type = 'image/gif';                break;
-	case '.jpg'  : $f_type = 'image/jpeg';               break;
-	case '.png'  : $f_type = 'image/png';                break;
-	case '.ico'  : $f_type = 'image/x-icon';             break;
-	// documents
-	case '.js'   : $f_type = 'application/x-javascript'; break;
-	case '.css'  : $f_type = 'text/css';                 break;
-	case '.xml'  : $f_type = 'text/xml';                 break;
-	case '.json' : $f_type = 'application/json';         break;
-	// no match
-	default      :
-		// extension is not supported, issue *403*
-		header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden');
-		exit('<h1>403 Forbidden</h1>');
-}
-
 //exit('<h1>200 OK</h1>'); // STOP here
 
 // construct usable file path
-$f_path = './cache/'.$f_name.$f_ext;
+$cache_path = './cache/'.$f_name.$f_ext;
 if(!file_exists('./cache/')){
 	// we have to create the folder first
 	if(mkdir('./cache/')){
@@ -102,12 +102,70 @@ if(!file_exists('./cache/')){
 	}
 }
 
-function fetch_file($f_remote_uri, $f_path, $f_modified){
-	// http *HEAD* request 
-	// verify that the file exists
+function fetch_file($server_uri){
+	$ch = curl_init();
+	curl_setopt_array($ch, array(
+		CURLOPT_URL            => $server_uri,
+		CURLOPT_TIMEOUT        => 15, // it is a short time for a background service, right?
+		CURLOPT_CONNECTTIMEOUT => 5,
+		CURLOPT_FAILONERROR    => 1,
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_BINARYTRANSFER => 1,
+		CURLOPT_HEADER         => 1,
+		CURLOPT_FOLLOWLOCATION => 1
+	));
+	
+	$resp = curl_exec($ch);
+	if($resp === false) { // error handling
+		return array('status'=>false, 'error'=>curl_error($ch));
+	}
+
+	$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+	$header = substr($resp, 0, $header_size);
+	$contents = substr($resp, $header_size);
+
+	return array('status'=>true, 'header'=>$header, 'contents'=>$contents, 'header_size'=>$header_size);
+}
+
+/**
+ * @param mixed $fp 要识别的字符串或文件指针
+ * @param int $header_length 如果 $fp 中同时带有 header 和内容，要自动截取出 header 部分的长度
+ *
+ * @return int Unix 标准时间戳; 如果识别失败返回 -1
+ */
+function get_mtime_from_fp($fp, $header_length = 0){
+	if(is_resource($fp)){
+		$contents = '';
+		rewind($fp);
+		if($header_length > 0){
+			$contents = fread($fp, $header_length);
+		}else{
+			while(!feof($handle)) {
+				$contents .= fread($fp, 8192);
+			}
+		}
+		rewind($fp);
+	}else{
+		$contents = $fp;
+	}
+	if($header_length > 0){
+		$contents = substr($contents, 0, $header_length);
+	}
+
+	preg_match('/Last-Modified:(.*?)\n/', $contents, $matches);
+	$result = trim(array_pop($matches));
+	return $result ? strtotime($result) : -1;
+}
+
+function redirect2remote($uri){
+	header('Location: ' . $uri, true, 302);
+	exit();
+}
+
+function fetch_file_mtime($server_uri){
 	$ch = curl_init();
 	curl_setopt_array($ch, 	array(
-		CURLOPT_URL            => $f_remote_uri,
+		CURLOPT_URL            => $server_uri,
 		CURLOPT_TIMEOUT        => 10,
 		CURLOPT_CONNECTTIMEOUT => 5,
 		CURLOPT_FAILONERROR    => 1,
@@ -115,106 +173,120 @@ function fetch_file($f_remote_uri, $f_path, $f_modified){
 		CURLOPT_BINARYTRANSFER => 1,
 		CURLOPT_HEADER         => 1,
 		CURLOPT_NOBODY         => 1,
-		CURLOPT_FOLLOWLOCATION => 1, 
+		CURLOPT_FOLLOWLOCATION => 1
 	));
 	
 	$header = curl_exec($ch);
-	if ($header !== false) { // remote file exists
-		preg_match('/Last-Modified:(.*?)\n/', $header, $matches); 
-		$remote_time = strtotime(trim(array_pop($matches)));
+	if($header){
+		return get_mtime_from_fp($header);
+	}else{
+		return -2;
+	}
+}
 
-		if($remote_time > $f_modified){
-			$fp = fopen($f_path, 'a+b'); // append and binary mode
-			if(flock($fp, LOCK_EX | LOCK_NB)) {
-				// empty *possible* contents
-				ftruncate($fp, 0);
-				rewind($fp);
+function get_cache_md5($path){
+	return file_get_contents($path.'.md5');
+}
 
-				// http *GET* request
-				// and write directly to the file
-				$ch2 = curl_init();
-				curl_setopt_array($ch2, 	array(
-					CURLOPT_URL            => $f_remote_uri,
-					CURLOPT_TIMEOUT        => 15, // it is a short time for a background service, right?
-					CURLOPT_CONNECTTIMEOUT => 5,
-					CURLOPT_FAILONERROR    => 1,
-					CURLOPT_RETURNTRANSFER => 1,
-					CURLOPT_BINARYTRANSFER => 1,
-					CURLOPT_HEADER         => 0,
-					CURLOPT_FILE           => $fp
-					// CURLOPT_FOLLOWLOCATION => 1, 
-				));
-					
-				// did the transfer complete?
-				if (curl_exec($ch2) === false) {
-					// something went wrong, null 
-					// the file just in case >.>
-					//ftruncate($fp, 0); 
-				}
-				
-				// 1) flush output to the file
-				// 2) release the file lock
-				// 3) release the curl socket
-				fflush($fp);
-				flock($fp, LOCK_UN);
-				curl_close($ch2);
-			}
-					
-			// close the file
-			fclose($fp);
-				
-			
+function upload2remote($remote_uri, $contents, $md5 = null){
+	global $upyun;
+	try{
+		if($md5){
+			$opts = array(UpYun::CONTENT_MD5 => $md5);
 		}else{
-			touch($f_path, $remote_time); // Change the modified time to resign the cache expire
+			$opts = array();
 		}
+		$res = $upyun->writeFile($remote_uri, $contents, true, $opts);
+		return array('status' => $res);
+	}catch(Exception $e){
+		return array('status' => false, 'error' => $e->getMessage(), 'code' => $e->getCode());
+	}
+}
 
-		// issue *302* for *this* request
-		header('Location: ' . $f_remote_uri, true, 302);
-		
-		curl_close($ch);
-		exit(); // STOP here
-	} else {
+function save_cache($path, $md5, $mtime = 0){
+	$r = file_put_contents($path.'.md5', $md5, LOCK_EX);
+	if($mtime > 0){
+		touch($path.'.md5', $mtime);
+	}
+	return $r;
+}
+
+// check the local cache
+if (!$force_sync && file_exists($cache_path.'.md5')) {
+	// get last modified time
+	$f_modified = filemtime($cache_path.'.md5');
+
+	$f_expires = (int) $f_matched[2] ? (int) $f_matched[2] : $f_expires_default;
+	//var_dump($f_modified,  $f_matched, $f_expires, time());
+	if($f_matched[2] != 'f' && $f_modified + $f_expires < time()){//expired
+		if($f_matched[2] == 'n'){ // ignore the mtime check
+			$server_mtime = -1;
+		}else{
+			$server_mtime = fetch_file_mtime($server_uri);
+		}
+		if($server_mtime == -2){
+			// the file doesn't exist, issue *404*
+			// *** TODO: we may cache the 404 result to prevent abuse
+			header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+			header('Cache-Control: private');
+			exit('<h1>404 Not Found</h1>');
+		}elseif($server_mtime == -1 || $server_mtime > $f_modified){ // the server is newer; we should fetch the content
+			$server_file = fetch_file($server_uri);
+			if($server_file['status']){
+				$server_md5 = md5($server_file['contents']);
+
+				if($server_md5 == get_cache_md5($cache_path)){ // the file is the same one; just change the modified time to resign the cache expire
+					touch($cache_path.'.md5'); // the modified time from the server is useless now
+					header('X-Checker-Status: Same');
+				}else{ // sync the file to the remote cdn
+					save_cache($cache_path, $server_md5);
+					$remote_res = upload2remote($remote_uri, $server_file['contents'], $server_md5);
+					if($remote_res['status']){
+						header('X-Checker-Status: Synced');
+					}else{
+						header($_SERVER['SERVER_PROTOCOL'] . ' 500 Server Error');
+						exit('<h1>'.$remote_res['code'].' '.$remote_res['error'].'</h1>');
+					}
+				}
+			}else{
+				// the file doesn't exist at this time, issue *404*
+				// when ignoring mtime check, the file exitance check is left here;
+				// otherwise it would not occur unless you are in bad luck :(
+				// *** TODO: we may cache the 404 result to prevent abuse
+				header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+				header('Cache-Control: private');
+				exit('<h1>404 Not Found</h1>');
+			}
+		}else{ // just change the modified time to resign the cache expire
+			touch($cache_path.'.md5');
+			header('X-Checker-Status: Fresh');
+		}
+		redirect2remote($remote_publicuri);
+	}else{ // don't check the server for changes
+		header('X-Checker-Status: No-Expired');
+		redirect2remote($remote_publicuri);
+	}
+}else{
+	// no cache yet? fetch it first
+	$server_file = fetch_file($server_uri);
+	if(!$server_file['status']){
 		// the file doesn't exist, issue *404*
 		// *** TODO: we may cache the 404 result to prevent abuse
 		header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
 		header('Cache-Control: private');
-	}
-	
-	// finished
-	curl_close($ch);
-}
+		exit('<h1>404 Not Found</h1>');
+	}else{
+		// save and sync it
+		$server_md5 = md5($server_file['contents']);
 
-function output_file($f_path, $f_modified, $f_type, $f_expires){
-	// validate the client cache
-	if (isset(    $_SERVER['HTTP_IF_MODIFIED_SINCE']) && 
-	   (strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $f_modified)
-	) {
-		// client has a valid cache, issue *304*
-		header($_SERVER['SERVER_PROTOCOL'] . ' 304 Not Modified');
-	} else {
-		// send all requisite cache-me-please! headers
-		header('Pragma: public');
-		header('Cache-Control: max-age=' . $f_expires);
-		header('Content-type: ' . $f_type);
-		header('Last-Modified: ' . gmdate('D, d M Y H:i:s \G\M\T', $f_modified));
-		header('Expires: ' . gmdate('D, d M Y H:i:s \G\M\T', time() + $f_expires));
-		
-		// stream the file
-		readfile($f_path);
+		save_cache($cache_path, $server_md5, get_mtime_from_fp($server_file['header']));
+		$remote_res = upload2remote($remote_uri, $server_file['contents'], $server_md5);
+		if($remote_res['status']){
+			header('X-Checker-Status: Created');
+			redirect2remote($remote_publicuri);
+		}else{
+			header($_SERVER['SERVER_PROTOCOL'] . ' 500 Server Error');
+			exit('<h1>'.$remote_res['code'].' '.$remote_res['error'].'</h1>');
+		}
 	}
-}
-
-// check the local cache
-if (file_exists($f_path)) {
-	// get last modified time
-	$f_modified = filemtime($f_path);
-
-	$f_expires = (int) $f_matched[2] ? (int) $f_matched[2] : $f_expires_default;
-	if($f_matched[2] != 'f' && $f_modified + $f_expires < time()){//expired
-		fetch_file($f_remote_uri, $f_path, $f_modified);
-	}else{ // don't check the remote for changes
-		output_file($f_path, $f_modified, $f_type, $f_expires);
-	}
-}else{ // no cache yet? fetch it first
-	fetch_file($f_remote_uri, $f_path, 0);
 }
